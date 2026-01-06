7d2cf4f8016099ac34bdbfe0b9d7699d6907541d
ckt <cacao.chen@heavengifts.com>
2026-01-06 18:31:12 +0800

    feat： 背景图片刷新抖动问题实现

---
+++ src/components-style/BackgroundShake/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './index.less'
+
+const Index = () => {
+  return (
+    <div className="background-shake">
+      <section className="step1">
+        <button>Let's Get Started！</button>
+        <img src="https://img.xjh.me/desktop/nature/59411146_p0.jpg" alt="step1" />
+      </section>
+
+      <section className="step2">
+        <p>step2</p>
+        <img src="https://img.xjh.me/desktop/acg/34844544_p0.jpg" alt="step2" />
+      </section>
+    </div>
+  )
+}
+
+export default Index
