b4c2e6eb63450260af0c5637165d7c35198073c4
ckt <cacao.chen@heavengifts.com>
2026-01-13 16:33:28 +0800

    feat: 多行文本展示不下显示省略号方案

--- src/components-style/animation.jsx
+++ src/components-style/animation.jsx
@@ -4,7 +4,7 @@ import Headerbox from '@/components/headerbox/index.jsx';
 let list = [
   {
     title: 'canvas绘制基础图形详解',
-    path: '/3dAnimation/photoAlbum.html',
+    path: '/animation/photoAlbum.html',
   },
 ];
 
--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,52 +1,59 @@
-import React from 'react'
-import { TabBar } from 'antd-mobile'
-import styles from './index.less'
-import Style from '@/views/style.jsx'
-import Function from '@/views/function.jsx'
-import Frame from '@/views/frame.jsx'
-import Todo from '@/views/todo.jsx'
-import { useLocation, useNavigate } from 'react-router-dom'
-import { AppOutline, MessageOutline, UnorderedListOutline, UserOutline } from 'antd-mobile-icons'
+import React from 'react';
+import { TabBar } from 'antd-mobile';
+import styles from './index.less';
+import Style from '@/views/style.jsx';
+import Function from '@/views/function.jsx';
+import Frame from '@/views/frame.jsx';
+import Todo from '@/views/todo.jsx';
+import { useLocation, useNavigate } from 'react-router-dom';
+import {
+  AppOutline,
+  MessageOutline,
+  UnorderedListOutline,
+  UserOutline,
+} from 'antd-mobile-icons';
 const tabs = [
   {
     key: '/frame',
     title: '框架',
     icon: <AppOutline />,
-    components: <Frame />
+    components: <Frame />,
   },
   {
     key: '/todo',
     title: '待办',
     components: <Todo />,
-    icon: <UnorderedListOutline />
+    icon: <UnorderedListOutline />,
   },
   {
     key: '/function',
     title: '功能',
     components: <Function />,
-    icon: <MessageOutline />
+    icon: <MessageOutline />,
   },
   {
     key: '/style',
     title: '样式',
     components: <Style />,
-    icon: <UserOutline />
-  }
-]
+    icon: <UserOutline />,
+  },
+];
 
 const Nav = () => {
-  const location = useLocation()
-  const { pathname } = location
-  const navigate = useNavigate()
+  const location = useLocation();
+  const { pathname } = location;
+  const navigate = useNavigate();
   const setRouteActive = value => {
-    navigate(value)
+    navigate(value);
     // 设置文档标题
-    let title = tabs.find(i => i.key === value).title
-    document.title = title
-  }
+    let title = tabs.find(i => i.key === value).title;
+    document.title = title;
+  };
   return (
     <div className={styles.app}>
-      <div className={styles.body}>{tabs.find(item => item.key === pathname).components}</div>
+      <div className={styles.body}>
+        {tabs.find(item => item.key === pathname).components}
+      </div>
       <div className={styles.bottom}>
         <TabBar activeKey={pathname} onChange={setRouteActive}>
           {tabs.map(item => (
@@ -55,7 +62,7 @@ const Nav = () => {
         </TabBar>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
