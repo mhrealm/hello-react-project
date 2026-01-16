837ba4f4a7d7ad50f64fc95cd861e60e83cd198b
ckt <cacao.chen@heavengifts.com>
2026-01-16 11:28:39 +0800

    feat: 网易云七夕活动复刻第二版

--- src/components-function/questionAnswerSession/index.jsx
+++ src/components-function/questionAnswerSession/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import FirstEdition from './firstEdition/index.jsx';
 import SecondEdition from './secondEdition/index.jsx';
+import ThirdEition from './thirdEdition/index.jsx';
 
 const Index = () => {
   return (
     <>
       {/* <FirstEdition /> */}
-      <SecondEdition />
+      {/* <SecondEdition /> */}
+      <ThirdEition />
     </>
   );
 };
